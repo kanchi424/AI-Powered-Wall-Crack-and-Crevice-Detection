5560efcc6ef8ada320417332f4defcb2aca1aa57
kanchi424 <64429781+kanchi424@users.noreply.github.com>
2024-09-03 20:12:53 +0530

    Training.py
    Code used for training the model. I have trained the model for 10  epochs.It took about one hour ten minutes.

---
+++ Training.py
@@ -0,0 +1,7 @@
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // BATCH_SIZE,
+    epochs=10  # Adjust the number of epochs as needed
+)
